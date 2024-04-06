823418526d155e9b2608f66b44445980b7962367
홍 <icandogo1@naver.com>
2024-04-06 15:24:57 +0900

    bookshorts update list.js, list.html (점점점 버튼 클릭시 '수정하기','삭제하기','공유하기') 모달 추가

---
+++ src/main/resources/static/js/shorts/list.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+    const dropdownButtons = document.querySelectorAll(".dropdown-btn");
+    const dropdownLists = document.querySelectorAll(".dropdown-list");
+
+    dropdownButtons.forEach((dropdownButton, index) => {
+        dropdownButton.addEventListener("click", () => {
+            dropdownLists[index].classList.toggle("active");
+        });
+    });
+});
