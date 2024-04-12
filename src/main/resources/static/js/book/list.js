2081574fc21444b928e24257d03f0156db49578d
daback <daback93@gmail.com>
2024-04-12 09:38:16 +0900

    Comment 책 리스트 책 검색 js 주석 추가

--- src/main/resources/static/js/book/list.js
+++ src/main/resources/static/js/book/list.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function(){
 
+    // 검색을 위한 부모 form 찾기
     const searchBookForm = this.document.querySelector("#search-book-form");
     
     // 쿼리 검색
@@ -9,45 +10,46 @@ window.addEventListener("load", function(){
     // 카테고리 검색
     const searchCategory = searchBookForm.querySelector(".search-category");
 
+    // 검색 결과 list를 뿌려줄 컨텐츠 ul ( li로 리스트가 뿌려짐 )
     const contentUl = this.document.querySelector(".content-ul");
-   
-  
+    
+    //  쿼리 검색시에도 카테고리 선택값이 필요하므로 전역 변수 생성
+    let categoryValue;
+ 
+
+    // ===================================== 쿼리로 검색하는 경우 =======================================
+    // =================================================================================================
+
+    // 쿼리 입력 후 검색 버튼 클릭 or 엔터 클릭 시 실행
     queryInputBtn.addEventListener("click", async function(e){
         e.preventDefault();
-        // 쿼리로 검색하는 경우
 
         // 사용자가 검색을 눌렀을때 검색어
         let queryValue = searchQueryInput.value;
-        // console.log("뭔데???", queryValue);
-        // console.log("뭔데???", typeof queryValue);
-        // console.log("뭔데???",  queryValue === '');
 
-        //마가져와라
+        // 쿼리 검색 시 카테고리 선택 없는 경우 undefined 값처리
         if(categoryValue === undefined)
             categoryValue = null;
+        
+        // fetch 통신 후 response 받는 절
         let response = await getByParams(queryValue, categoryValue);
 
-        // if(response.status != 200){
-        //     alert("없는데용 ㅜ")          
-        //     return;
-        // }
-        
+        // 서버에서 문제가 생긴 경우 return
         if(response.status != 200){
             contentUl.innerHTML= '';
             contentUl.innerHTML= `검색 결과가 없습니다 ...  유감.`;
             return;
         }
-        let list = await response.json();
-        console.log(list);
 
-     
+        let list = await response.json();
         await printBookList(list);
         
     });
 
 
-    let tmp;
-    let categoryValue;
+    // =================================== 카테고리로 검색하는 경우 =======================================
+    // ==================================================================================================
+    // 카테고리 클릭 시 실행
     searchCategory.addEventListener("click", async function(e){
 
         // 카테고리로 검색하는 경우
@@ -57,25 +59,21 @@ window.addEventListener("load", function(){
         if(clickedCategoryLi.tagName != "LI")
             return;
 
-        let tmp = searchCategory.querySelectorAll("li");
-
-        let categorySpan = clickedCategoryLi.querySelector("span");
+        // 모든 카테고리 li에서 br-color:main-5를 제거해주기 위해 SelecetorAll
+        let allLis = searchCategory.querySelectorAll("li");
         
-        for(let t of tmp){
-            let otherSpanValue = t.querySelector("span").dataset.id;
-            let clickedSpanValue = categorySpan.dataset.id;
-
-            // 전체 li span의 dataset id값이 클릭된 li span의 id값과 다른 경우
+        
+        for(let a of allLis){
+            // 모든 카테고리 li와 span의 style 원래대로 돌려놓기
             // 기본스타일로 변경해준다.
-            if(otherSpanValue != clickedSpanValue)
-                t.classList.remove("bg-color:main-5");
-                t.classList.add("bg-color:main-1");
-                t.querySelector("span").classList.remove("color:base-1");
-
+            a.classList.remove("bg-color:main-5");
+            a.classList.add("bg-color:main-1");
+            a.querySelector("span").classList.remove("color:base-1");
         }
-
+        
         // 클릭된 li태그와 span에 색상 주기
-
+        // 클릭이벤트 발생한 li에서 클래스를 넣어주기 위한 span 태그 찾기
+        let categorySpan = clickedCategoryLi.querySelector("span");
         categorySpan.classList.add("color:base-1");
         clickedCategoryLi.classList.remove("bg-color:main-1");
         clickedCategoryLi.classList.add("bg-color:main-5");
@@ -84,21 +82,24 @@ window.addEventListener("load", function(){
         categoryValue = categorySpan.dataset.id;
         let queryValue = searchQueryInput.value;
 
-        //마 가져와라
+        //비동기 fetch 메소드 호출 및 GET 통신
         let response = await getByParams(queryValue, categoryValue);
 
+        // server에서 문제 생긴 경우 return
         if(response.status != 200){
             contentUl.innerHTML= '';
             contentUl.innerHTML= `검색 결과가 없습니다 ...  유감.`;
             return;
         }
 
-        let list = await response.json();
 
+        let list = await response.json();
         await printBookList(list);
         
     })
     
+    // ================================= 파라미터 추가하여 서버와 통신  =======================================
+    // ======================================================================================================
     function getByParams(q, c){
         
         const url = `/api/book/list?q=${q}&c=${c}`;
@@ -106,6 +107,8 @@ window.addEventListener("load", function(){
         return fetch(url);
     }
 
+    // ====================================== 검색결과 list를 출력 ===========================================
+    // =====================================================================================================
     function printBookList(list){
         contentUl.innerHTML= '';
 
