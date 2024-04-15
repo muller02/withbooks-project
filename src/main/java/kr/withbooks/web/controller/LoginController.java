dcfd3d47f91ac40dde98055e3b0e659b9dd78abc
K19619 <you@example.com>
2024-04-15 18:25:03 +0900

    spring security

--- src/main/java/kr/withbooks/web/controller/LoginController.java
+++ src/main/java/kr/withbooks/web/controller/LoginController.java
@@ -1,24 +1,26 @@
 package kr.withbooks.web.controller;
 
 
+import java.util.Enumeration;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
-
 public class LoginController {
 
     @GetMapping("login")
-    public  String login(){
+    public  String login(HttpServletRequest request){
+
+        
+      
 
         return  "login/login";
     }
 
-    @GetMapping("join")
-    public String join(){
+  
 
-
-        return "login/join";
-    }
 }
