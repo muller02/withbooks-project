ba4435d1ddbac70ad5bb54b536b4226672e8b02a
daback <daback93@gmail.com>
2024-04-16 16:36:38 +0900

    fix 책 리스트 preventDefault() 수정중

--- src/main/resources/static/js/bookmark/list.js
+++ src/main/resources/static/js/bookmark/list.js
@@ -11,6 +11,8 @@ const bookMarkSection = this.document.querySelector("#bookmark-section");
 const bookMarkLists = bookMarkSection.querySelectorAll(".bookmark-list");
 const bookCheckAllInput = bookCheckAll.querySelector("input");
 
+const aLinks = document.querySelectorAll("a");
+
 // 삭제버튼 눌렀을 때
 deleteBtn.onclick = function(){
 
@@ -20,46 +22,32 @@ deleteBtn.onclick = function(){
     // 전체 선택 시 체크박스 뜨게 하기
     let checkboxes = document.querySelectorAll("input");
     checkboxes.forEach((checkbox)=>{checkbox.classList.remove("d:none")});
-
-    // 모든 a링크를 못누르게 처리하고
-    // let bookALinks = bookMarkSection.querySelectorAll("a");
-    // bookALinks.forEach(
-    //     (aLink)=>{
-    //         aLink.onclick = (e)=>e.preventDefault();
-    //     }
-    // );
     
     // book-section을 눌렀을때 해당 책의 체크박스가 눌러지도록 조치
     // XXX 리팩토링 필요
     bookMarkSection.onclick = function(e){
-        console.log(e);
-        console.log(e.target);
-        e.preventDefault();
+
+        // e.preventDefault();
+
+        //  클릭한 책의 부모 section 찾기
+        let clickedElement = e.target;
+        let parentSection = clickedElement.parentElement;
+        
+        for(let i = 1 ;i<10;i++){
+            if(parentSection.tagName == "SECTION")
+                break;
+
+            parentSection = parentSection.parentElement;
+        }
+    
+        // 찾은 section의 checkbox 찾기
+        let checkbox = parentSection.querySelector("input");
+    
+        // checkbox의 boolean을 찾아 반대로 대입해주기
+        let tmp = checkbox.checked;
+        checkbox.checked =! tmp;
 
     };
-
-    // bookMarkLists.forEach((list)=>list.onclick = function(e){
-        
-    //     e.preventDefault();
-
-    //     // 클릭한 책의 부모 section 찾기
-    //     let clickedElement = e.target;
-    //     let parentSection = clickedElement.parentElement;
-        
-    //     for(let i = 1 ;i<10;i++){
-    //         if(parentSection.tagName == "SECTION")
-    //             break;
-
-    //         parentSection = parentSection.parentElement;
-    //     }
-    
-    //     // 찾은 section의 checkbox 찾기
-    //     let checkbox = parentSection.querySelector("input");
-    
-    //     // checkbox의 boolean을 찾아 반대로 대입해주기
-    //     let tmp = checkbox.checked;
-    //     checkbox.checked =! tmp;
-    // });
 };
 
 
@@ -89,9 +77,9 @@ checkAllBox.onchange = function(e){
     
     // 취소 버튼 숨김
     cancelBtn.classList.add("d:none");
-
-    // prevented된 a링크들 되살리기..
-
+    
+    bookMarkSection
+        
 
   };
 
