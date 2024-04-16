3e778c95d2ea0e2cf263e61e8d169edfb5d067c5
nayeon <keemnayn@gmail.com>
2024-04-16 17:09:37 +0900

    feat: with 목록

--- src/main/java/kr/withbooks/web/service/WithService.java
+++ src/main/java/kr/withbooks/web/service/WithService.java
@@ -1,16 +1,13 @@
 package kr.withbooks.web.service;
 
 import kr.withbooks.web.entity.With;
+import kr.withbooks.web.entity.WithView;
 
 import java.util.List;
 
 public interface WithService {
+    List<WithView> getList();
 
-
-
-    List<With> getList();
-    List<With> getList(Long[] categoryId);
-
-
+    // List<With> getList(Long[] categoryId);
 
 }
