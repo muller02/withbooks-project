9dab276dca216d44713d2125ebf0cf6e9f57f2b8
doihchiro <doihchiro@gmail.com>
2024-04-17 23:00:16 +0900

    debateBoard add

--- src/main/java/kr/withbooks/web/controller/with/debate/board/WithDebateBoardController.java
+++ src/main/java/kr/withbooks/web/controller/with/debate/board/WithDebateBoardController.java
@@ -1,5 +1,7 @@
 package kr.withbooks.web.controller.with.debate.board;
 
+import kr.withbooks.web.entity.DebateBoardView;
+import kr.withbooks.web.service.DebateBoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,15 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/with/debate/board")
 public class WithDebateBoardController {
 
+    @Autowired
+    private DebateBoardService debateBoardService;
+
     @GetMapping("/list")
-    public String list(@RequestParam(name = "") Long id, Model model) {
+    public String list(@RequestParam(name = "rid") Long id, Model model) {
 
+        List<DebateBoardView> list = debateBoardService.getList(id);
+        System.out.println("list: " + list);
+        model.addAttribute("list", list);
 
-
-        return "null";
+        return "with/debate/board/list";
     }
 }
---
+++ src/main/java/kr/withbooks/web/repository/DebateBoardViewRepository.java
@@ -0,0 +1,12 @@
+package kr.withbooks.web.repository;
+
+import kr.withbooks.web.entity.DebateBoardView;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DebateBoardViewRepository {
+
+    List<DebateBoardView> findAllById(Long id);
+}
