c3fd91e3911f3003c0d9e075a893c19ec0846f7b
K19619 <you@example.com>
2024-04-24 17:03:25 +0900

    feat: select 박스 기능 구현

--- src/main/java/kr/withbooks/web/service/DebateTopicService.java
+++ src/main/java/kr/withbooks/web/service/DebateTopicService.java
@@ -1,8 +1,12 @@
 package kr.withbooks.web.service;
 
+import java.util.List;
+
 import kr.withbooks.web.entity.DebateTopic;
 
 public interface DebateTopicService {
 
     DebateTopic getById(Long id);
+
+    List<DebateTopic> getList(Long roomId);
 }
--- src/main/java/kr/withbooks/web/service/DebateTopicServiceImp.java
+++ src/main/java/kr/withbooks/web/service/DebateTopicServiceImp.java
@@ -2,6 +2,9 @@ package kr.withbooks.web.service;
 
 import kr.withbooks.web.entity.DebateTopic;
 import kr.withbooks.web.repository.DebateTopicRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +12,18 @@ import org.springframework.stereotype.Service;
 public class DebateTopicServiceImp implements DebateTopicService {
 
     @Autowired
-    private DebateTopicRepository debateTopicRepository;
+    private DebateTopicRepository repository;
 
     @Override
     public DebateTopic getById(Long id) {
 
-        DebateTopic debateTopic = debateTopicRepository.findById(id);
+        DebateTopic debateTopic = repository.findById(id);
         return debateTopic;
+    }
+
+    @Override
+    public List<DebateTopic> getList(Long roomId) {
+        List<DebateTopic> list = repository.findAll(roomId);
+        return list;
     }
 }
