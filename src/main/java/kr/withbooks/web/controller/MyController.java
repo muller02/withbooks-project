63a1fcadea1b7a89ff81a8987ef110ab930acc64
K19619 <you@example.com>
2024-04-29 18:21:40 +0900

    feat: 알림 목록 구현중

--- src/main/java/kr/withbooks/web/controller/MyController.java
+++ src/main/java/kr/withbooks/web/controller/MyController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.withbooks.web.entity.User;
+import kr.withbooks.web.service.NotificationService;
 import kr.withbooks.web.service.UserService;
 
 @Controller
@@ -15,6 +16,9 @@ public class MyController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private NotificationService notificationService;
     
     @GetMapping("/index")
     public String index(
@@ -25,10 +29,7 @@ public class MyController {
         // userId = userDetails.getId();
         User user = userService.getById(userId);
 
-        // 근데 유저의 알림의 수를  NotificationService 에게 얻어오는게 맞나? 유저의 알림 관련이니까 UserService에서 해야하는거 아닌가?
-        // 그래서 NotificationService대신 UserService에게 얻어왔다.(질문 후 수정필요하면 수정하자)
-        // int notiCnt = notificationService.getNotiCount();
-        int notiCnt = userService.getNotiCount(userId);
+        int notiCnt = notificationService.getNotiCount(userId);
         
         model.addAttribute("user", user);
         model.addAttribute("notiCnt", notiCnt);
