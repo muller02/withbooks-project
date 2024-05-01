8cd873788b293251681b472cc12c8ec42ecb7edc
doihchiro <doihchiro@gmail.com>
2024-05-01 16:06:38 +0900

    feat 엔티티추가

---
+++ src/main/java/kr/withbooks/web/entity/DebateCommentView.java
@@ -0,0 +1,23 @@
+package kr.withbooks.web.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DebateCommentView {
+
+    private Long id;
+    private Long boardId;
+    private Long userId;
+    private String content;
+    private LocalDateTime regDate;
+    private int blindYn;
+    private String userNickname;
+}
