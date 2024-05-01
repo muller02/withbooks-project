bfdcb11f2211f099f256dcaee3241d4ad1657d74
cho <whdmsdo1010@gmail.com>
2024-05-01 10:35:11 +0900

    feat : 북로그 (detail 비동기 저장, reg 이미 저장된 북로그의 책 선택시 필터 적용)

--- src/main/java/kr/withbooks/web/controller/api/BooklogController.java
+++ src/main/java/kr/withbooks/web/controller/api/BooklogController.java
@@ -1,11 +1,13 @@
 package kr.withbooks.web.controller.api;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +17,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import kr.withbooks.web.entity.BooklogLogs;
 import kr.withbooks.web.service.BooklogService;
 
-@RequestMapping("api/booklog")
 @RestController("apiBooklogController")
+@RequestMapping("api/booklog")
 public class BooklogController {
 
     @Autowired
     private BooklogService service;
 
+    // 공개-비공개 버튼 클릭시 업데이트
     @GetMapping
-    public int changePublic(Long booklogId, Long publicYn
-            ){
+    public int changePublic(
+        Long booklogId
+        , Long publicYn
+        ){
 
         // Long userId = 4L;
         int result = 0;
@@ -32,40 +37,60 @@ public class BooklogController {
         result = service.changePublic(booklogId, publicYn);
         
         return result;
+    } 
+
+    // 새 로그 작성
+    @PostMapping("reg")
+    public BooklogLogs reg(
+        @RequestParam(name="content",required = false) String content,
+        @RequestParam(name="booklogId") Long id,
+        @RequestParam(name="file",required = false) MultipartFile imgFile,
+        HttpServletRequest request,
+        BooklogLogs logs) throws IOException{
+            
+            // 내용 없는 경우 null 대입
+            if(content.length() == 0)
+                content = null;
+            
+            String fileName = null;
+            // 파일 저장
+            if(imgFile != null && !imgFile.isEmpty())
+            {
+                fileName = imgFile.getOriginalFilename();
+
+                String path = "/image/booklog";
+
+                String realPath = request.getServletContext().getRealPath(path);
+
+                File pathFile = new File(realPath);
+
+                if(!pathFile.exists())
+                    pathFile.mkdirs();
+
+                File file = new File(realPath+File.separator+fileName);
+                imgFile.transferTo(file);
+            }
+
+            logs = BooklogLogs.builder()
+                        .content(content)
+                        .img(fileName)
+                        .booklogId(id)
+                        .build();
+
+            service.addLogs(logs);
+            
+            return logs;
     }
 
-    @PostMapping("reg")
-    public int reg(
-        // @RequestParam(name = "new-file", required = false) MultipartFile imgFile, 
-        @RequestBody BooklogLogs logs,
-        @RequestParam(required = false) MultipartFile file,
-        HttpServletRequest request) throws IOException{
+    @PostMapping("deleteByLogId")
+    public int delete(@RequestParam(name = "logs-id") Long logId ){
 
-            System.out.println(file);
+        System.out.println("logId : " + logId);
 
-            // 파일 저장
-            // if(imgFile != null && !imgFile.isEmpty())
-            // {
-            //     fileName = imgFile.getOriginalFilename();
+        int result = service.deleteLog(logId);
+        
+        return result;
 
-            //     String path = "/image/booklog";
-
-            //     String realPath = request.getServletContext().getRealPath(path);
-
-            //     File pathFile = new File(realPath);
-
-            //     if(!pathFile.exists())
-            //         pathFile.mkdirs();
-
-            //     File file = new File(realPath+File.separator+fileName);
-            //     imgFile.transferTo(file);
-            // }
-
-            // service.addLogs(logs);
-            System.out.println(logs.toString());
-
-            
-            return 0;
     }
     
 }
--- src/main/resources/static/js/booklog/list.js
+++ src/main/resources/static/js/booklog/list.js
@@ -1,3 +1,19 @@
-window.addEventListener("load", function(){
-    
-})
+window.addEventListener("load", function () {
+    let newLogBtn = this.document.querySelector("#new-log-reg");
+    let newLogBtnATag = newLogBtn.querySelector("a");
+    let bookList = this.document.querySelector(".book-list");
+    let bookIdList = bookList.querySelectorAll("input");
+
+    newLogBtnATag.onclick = function () {
+        // 책 아이디 로컬스토리지에 담기
+        let list = {};
+        let cnt = 0;
+
+        for (const id of bookIdList) {
+            list[cnt++] = id.value;
+        }
+
+        localStorage.setItem("bookids", JSON.stringify(list));
+
+    };
+});
