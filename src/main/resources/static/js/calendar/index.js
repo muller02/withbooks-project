ac971aba76f7c9b429557b754cc65639ee35bb78
nayeon <keemnayn@gmail.com>
2024-05-17 21:40:35 +0900

    feat: 캘린더 클릭 이벤트 적용

--- src/main/resources/static/js/calendar/index.js
+++ src/main/resources/static/js/calendar/index.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // calendar element 취득
-  let calendarEl = document.getElementById("calendar");
+  let calendarEl = document.querySelector("#calendar");
+  let modal = document.querySelector("#calendar-modal");
+  let selectedInfo = null; // 저장공간
+  let eventForm = document.getElementById("event-form");
+  let eventTitle = document.getElementById("event-title");
+  let eventStart = document.getElementById("event-start");
+  let eventEnd = document.getElementById("event-end");
+  let cancelBtn = document.getElementById("cancel-btn");
+
   if (calendarEl) {
     // full-calendar 생성하기
     let calendar = new FullCalendar.Calendar(calendarEl, {
@@ -8,55 +16,70 @@ document.addEventListener("DOMContentLoaded", () => {
       expandRows: true, // 화면에 맞게 높이 재설정
       slotMinTime: "08:00", // Day 캘린더에서 시작 시간
       slotMaxTime: "20:00", // Day 캘린더에서 종료 시간
+      defaultTimedEventDuration: "01:00:00", // 이벤트 기본 기간
+      now: new Date(), // 현재 시간
       // 해더에 표시할 툴바
       headerToolbar: {
         left: "prev,next today",
         center: "title",
         right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
       },
-      initialView: "dayGridMonth", // 초기 로드 될때 보이는 캘린더 화면(기본 설정: 달)
-      initialDate: "2024-05-14", // 초기 날짜 설정 (설정하지 않으면 오늘 날짜가 보인다.)
+      initialView: "dayGridMonth", // 초기 로드 될때 보이는 캘린더
       navLinks: true, // 날짜를 선택하면 Day 캘린더나 Week 캘린더로 링크
       editable: true, // 수정 가능?
       selectable: true, // 달력 일자 드래그 설정가능
       nowIndicator: true, // 현재 시간 마크
-      dayMaxEvents: true, // 이벤트가 오버되면 높이 제한 (+ 몇 개식으로 표현)
+      dayMaxEvents: true, // +more 표시 전 최대 이벤트 갯수, 셀 높이에 의해 결정
       locale: "ko", // 한국어 설정
-      eventAdd: (info) => {
-        // 이벤트가 추가되면 발생하는 이벤트
-        console.log(info);
+
+      // 클릭해서 이벤트 생성
+      dateClick: function (info) {
+        modal.classList.remove("d:none");
+        let selectedDate = info.dateStr;
+        eventStart.value = selectedDate;
+        eventEnd.value = selectedDate;
       },
-      eventChange: (info) => {
-        // 이벤트가 수정되면 발생하는 이벤트
-        console.log(info);
-      },
-      eventRemove: (info) => {
-        // 이벤트가 삭제되면 발생하는 이벤트
-        console.log(info);
-      },
-      select: (info) => {
-        // 캘린더에서 드래그로 이벤트를 생성할 수 있다.
-        let title = prompt("Event Title:");
-        if (title) {
-          calendar.addEvent({
-            title: title,
-            start: info.startStr,
-            end: info.endStr,
-            allDay: info.allDay,
-          });
-        }
-      },
-      // 이벤트
-      events: {
-        url: "/events",
-        extraParams: function () {
-          return {
-            dynamic_value: Math.random(),
-          };
-        },
+      // 드래그로 이벤트 생성
+      select: function (info) {
+        console.log("Start date: ", info.startStr);
+        console.log("End date: ", info.endStr);
+        // eventStart.value = info.startStr.split("T")[0];
+        // eventEnd.value = info.endStr.split("T")[0];
+        eventStart.value = info.dateStr;
+        eventEnd.value = info.dateStr;
+        modal.classList.remove("d:none");
       },
     });
-    // 캘린더 랜더링
     calendar.render();
+
+    // 등록 버튼 이벤트
+    eventForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      let title = eventTitle.value.trim();
+      let start = eventStart.value;
+      let end = eventEnd.value;
+      if (title && start && end) {
+        calendar.addEvent({
+          title: title,
+          start: start,
+          end: end,
+          // 종일 지속되는지 여부
+          allDay: selectedInfo ? selectedInfo.allDay : false,
+        });
+        modal.classList.add("d:none");
+        // 다음 이벤트를 위해 입력 필드 초기화
+        eventTitle.value = "";
+        eventStart.value = "";
+        eventEnd.value = "";
+      } else alert("모든 필드를 입력하세요.");
+    });
+
+    // 취소 버튼 이벤트
+    cancelBtn.addEventListener("click", () => {
+      modal.classList.add("d:none");
+      eventTitle.value = "";
+      eventStart.value = "";
+      eventEnd.value = "";
+    });
   }
 });
