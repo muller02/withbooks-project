07d9ca281f8d980d7f1c3efbc8950784a0f2b59d
hong <icandogo1@naver.com>
2024-05-19 22:51:11 +0900

    update: css

--- src/main/resources/static/js/layout.js
+++ src/main/resources/static/js/layout.js
@@ -30,14 +30,14 @@ window.addEventListener("load", function (e) {
                 bookSearchD.querySelector("a").classList.add("color:main-5","icon-color:main-5");
                 bookSearchD.classList.add("bg-color:main-2","border-radius:6","p:2");
 
-        }else if(queryMenu == 2){
+        }else if(queryMenu == 3){
                 withT.classList.add("bg-color:main-2","border-radius:6","p:3");
                 withT.querySelector("a").classList.add("color:main-5","icon-color:main-5")
 
                 withD.classList.add("bg-color:main-2","border-radius:6","p:3");
                 withD.querySelector("a").classList.add("color:main-5","icon-color:main-5")
 
-        }else if(queryMenu ==3 ){
+        }else if(queryMenu ==2){
                 bookShortsD.classList.add("bg-color:main-2","border-radius:6","p:3");
                 bookShortsD.querySelector("a").classList.add("color:main-5","icon-color:main-5")
 
