92481b990c9320474748fcf82e9dd6088d410494
daback <daback93@gmail.com>
2024-05-20 18:16:04 +0900

    feat: /bookshorts 페이징 구현중

--- src/main/java/kr/withbooks/web/controller/api/BookShortsController.java
+++ src/main/java/kr/withbooks/web/controller/api/BookShortsController.java
@@ -1,17 +1,49 @@
 package kr.withbooks.web.controller.api;
 
+import kr.withbooks.web.config.CustomUserDetails;
+import kr.withbooks.web.entity.BookshortsAttachment;
 import kr.withbooks.web.entity.BookshortsView;
+import kr.withbooks.web.service.BookshortsAttachmentService;
+import kr.withbooks.web.service.BookshrotsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticatedPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController("apiBookShortsController")
-@RequestMapping("/api/bookshorts")
+@RequestMapping("/api/bookShorts")
 public class BookShortsController {
 
+    @Autowired
+    private BookshrotsService service;
+    
+    @Autowired
+    private BookshortsAttachmentService shortsAttachmentService;
 
+    @GetMapping("list")
+    public List<BookshortsView> getList(
+                                    @AuthenticationPrincipal  CustomUserDetails userDetails,
+                                    @RequestParam(name = "id", required = false) Long bookId,
+                                    @RequestParam(name="ls") Long lastShortsId
+                                    ){
+        Long userId = 1L;
+        if(userDetails != null)
+            userId = userDetails.getId();
+
+        List<BookshortsView> list = service.getView(bookId, userId, lastShortsId);
+        
+        System.out.println("=================================================================");
+        System.out.println(list);
+
+        return list;
+    }
 
 }
--- src/main/java/kr/withbooks/web/service/BookshrotsService.java
+++ src/main/java/kr/withbooks/web/service/BookshrotsService.java
@@ -9,12 +9,13 @@ import kr.withbooks.web.entity.BookshortsView;
 public interface BookshrotsService {
 
     void add(Bookshorts shorts);
-    List<BookshortsView> getView(Long booId,Long userId);
+    List<BookshortsView> getView(Long booId,Long userId, int page);
     List<BookshortsView> getView();
     List<BookshortsAttachment> getAttach();
     Bookshorts get(Long shortsId);
     void delete(Long shortsId);
-    List<BookshortsView> getBestList(); 
+    List<BookshortsView> getBestList();
+    List<BookshortsView> getView(Long bookId, Long userId, Long lastShortsId); 
 
     
 }
