3edb0a86c7029de380223f238a5e29d76254c871
cho <whdmsdo1010@gmail.com>
2024-05-20 14:43:10 +0900

    feat : admin/user/detail 기능 추가중

--- src/main/java/kr/withbooks/web/repository/UserRepository.java
+++ src/main/java/kr/withbooks/web/repository/UserRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.withbooks.web.entity.Bookshorts;
+import kr.withbooks.web.entity.BookshortsView;
 import kr.withbooks.web.entity.User;
 
 @Mapper
@@ -24,5 +26,6 @@ public interface UserRepository {
 
     List<User> findByAll(Long id, String nickname, String email, String birthyear, Integer gender, String startDate,
             String endDate, Integer status);
+    List<BookshortsView> findByIdShorts(Long id);
 
 }
