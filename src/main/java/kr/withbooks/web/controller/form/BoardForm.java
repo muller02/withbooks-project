f8bf5b5c80970b24f73b1816052f8b332220db65
doihchiro <doihchiro@gmail.com>
2024-05-23 16:33:17 +0900

    fix: debateBoard url mapping ì수정

--- src/main/java/kr/withbooks/web/controller/form/BoardForm.java
+++ src/main/java/kr/withbooks/web/controller/form/BoardForm.java
@@ -1,4 +1,4 @@
-package kr.withbooks.web.controller.with.debate;
+package kr.withbooks.web.controller.form;
 
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,12 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class BoardEditForm {
+public class BoardForm {
 
-    private Long id;
-    //private Long topicId;
+    private Long topicId;
     private String title;
     private String content;
     private List<MultipartFile> files = new ArrayList<>();
-    private List<Long> deleteFilesId = new ArrayList<>();
+
 }
